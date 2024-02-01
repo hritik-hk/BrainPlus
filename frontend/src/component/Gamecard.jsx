b09d1e7bcd0cd44752348ca67490e2f6a3031699
hritik-hk <kumar.hritik4w@gmail.com>
2024-02-01 20:41:21 +0530

    frontend files moved to frontend directory

--- frontend/src/component/Gamecard.jsx
+++ frontend/src/component/Gamecard.jsx
@@ -4,11 +4,11 @@ export default function Gamecard({data}){
         const svgCode = svg;
 
     return(
-    <div className=" bg-white rounded-md text-black w-[400px] h-[300px] flex flex-col justify-center items-center cursor-pointer">
+    <div className=" bg-white rounded-md text-black w-[300px] h-[280px] flex flex-col justify-center items-center cursor-pointer">
         <div className="w-[100px] h-[100px] m-2">
       <div dangerouslySetInnerHTML={{ __html: svgCode }} />
       </div>
-        <h3 className="text-5xl text-center font-medium m-2">{title}</h3>
+        <h3 className="text-4xl text-center font-medium m-2">{title}</h3>
         <p className="text-xl text-center m-2" >{description}</p>
     </div>
     )
